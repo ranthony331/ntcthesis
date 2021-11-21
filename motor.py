8d41c4ffe89b25c3160891c9291a0b91a772558a
Reuben Anthony <ranthony331@gmail.com>
2021-11-21 19:16:11 +0800

    Changed layout to make space for circuit image.

--- motor.py
+++ motor.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import RPi.GPIO as GPIO
 from time import sleep
 import os
@@ -41,7 +42,8 @@ master.configure(background='black')
 
 myFont = font.Font(family='Helvetica', size=20, weight='bold')
 smallFont = font.Font(family='Helvetica', size=15, weight='bold')
-
+OLFont = font.Font(family='Helvetica', size=10, weight='bold')
+titleFont = font.Font(family='Helvetica', size=23, weight='bold')
 
 
 Fwd_state = False
@@ -54,24 +56,25 @@ def FwdButton():
         FWDlabel['text'] = "Cannot start\nOL Detected"
     elif Fwd_state == True:
         print("FWD RUNNING ALREADY")
-        INFOlabel['text'] = "WARNING: Motor already in Forward run"     
+        INFOlabel['text'] = "WARNING:\nMotor already in Forward run"     
     elif Rev_state == False:
         GPIO.output(GPIO21_Fwd, GPIO.HIGH)
         Fwd_state = True
-        FWDlabel['text'] = "FORWARD RUNNING"
+        FWDlabel['text'] = "FORWARD RUN"
         REVlabel['text'] = "LOCKED"
-        print("FORWARD push button pressed, relay going ON")
-        INFOlabel['text'] = "COMMAND: Forward button pressed"
+        print("\nFORWARD SW ON\n")
+        INFOlabel['text'] = "COMMAND:\nForward button pressed"
         sleep(sleep_timer)
         GPIO.output(GPIO21_Fwd, GPIO.LOW)
-        print("Fwd relay going OFF")
+        print("Fwd SW OFF\n\n\n")
         #FWDlabel = tk.Label(master, text="REVERSE ON", bg="black", fg="green")
         #FWDlabel.grid(row=0, column=1)
         #FWDlabel['font'] = myFont
     elif Rev_state == True:
-        FWDlabel['text'] = "STOP MOTOR FIRST\nBEFORE FORWARDING"
+        REVlabel['text'] = "REVERSE RUNNING"
+        FWDlabel['text'] = "LOCKED"
         STOPlabel['text'] = "STOP MOTOR FIRST\nBEFORE CHANGING"
-        INFOlabel['text'] = "INVALID OPERATION: Motor must first be turned off\nbefore switching between Forward/Reverse"
+        INFOlabel['text'] = "INVALID OPERATION:\nMotor must first be turned off\nbefore switching Forward/Reverse"
         print("WARNING: REVERSE ALREADY RUNNING")
     else:
         print("UNHANDLED CONDITION in FWD FUNC")
@@ -82,25 +85,25 @@ def RevButton():
         REVlabel['text'] = "Cannot start\nOL Detected"
     elif Rev_state == True:
         print("REVERSE RUNNING ALREADY") 
-        INFOlabel['text'] = "WARNING: Motor already in Reverse run" 
+        INFOlabel['text'] = "WARNING:\nMotor already in Reverse run" 
     elif Fwd_state == False:
         GPIO.output(GPIO20_Rev, GPIO.HIGH)
         Rev_state = True
-        print("REVERSE push button pressed, relay going ON")
-        INFOlabel['text'] = "COMMAND: Reverse button pressed"
+        print("\n\nREVERSE ON")
+        INFOlabel['text'] = "COMMAND:\nReverse button pressed"
         sleep(sleep_timer)
         GPIO.output(GPIO20_Rev, GPIO.LOW)
-        print("Rev relay OFF")
+        print("Rev relay OFF\n\n")
         #REVlabel = tk.Label(master, text="REVERSE ON", bg="black", fg="green")
        # REVlabel['font'] = myFont
        # REVlabel.grid(row=1, column=1)
         FWDlabel['text'] = "LOCKED"
         REVlabel['text'] = "REVERSE RUN"
     elif Fwd_state == True:
-        FWDlabel['text'] = "REVERSE RUNNING"
-        REVlabel['text'] = "STOP MOTOR FIRST\nBEFORE REVERSING"
+        FWDlabel['text'] = "FORWARD RUNNING"
+        REVlabel['text'] = "LOCKED"
         STOPlabel['text'] = "STOP MOTOR FIRST\nBEFORE CHANGING"
-        INFOlabel['text'] = "INVALID OPERATION: Motor must first be turned off\nbefore switching between Forward/Reverse"
+        INFOlabel['text'] = "INVALID OPERATION:\nMotor must first be turned off\nbefore switching Forward/Reverse"
         print("WARNING: FORWARD ALREADY RUNNING")
     else:
         print("UNHANDLED CONDITION in REV FUNC")
@@ -112,16 +115,15 @@ def StopButton():
     GPIO.output(GPIO20_Rev, GPIO.LOW)
     GPIO.output(GPIO21_Fwd, GPIO.LOW)
     ### STOP RELAY WILL BE NC, SIGNAL WILL SWITCH TO NO
-    print("STOP button, relay going ON(NC TO NO)")
+    print("\n\nSTOP press")
     GPIO.output(GPIO26_Stop, GPIO.HIGH)
     master.after(2000, AfterTimerOff)
     #sleep(sleep_timer)
     #GPIO.output(GPIO26_Stop, GPIO.LOW)
-    print("Relay going off(NO to NC)")
     FWDlabel['text'] = "MOTOR STOP"
     REVlabel['text'] = "MOTOR STOP"
     STOPlabel['text'] = "MOTOR STOP"
-    INFOlabel['text'] = "WARNING: Please make sure the motor is \ncompletely stopped before changing reverse/forward"
+    INFOlabel['text'] = "WARNING:\nPlease make sure the motor is \ncompletely stopped before start"
     
   #  FWDlabel = tk.Label(master, text="MOTOR STOPPED", bg="black", fg="green")
   #  REVlabel = tk.Label(master, text="MOTOR STOPPED", bg="black", fg="green")
@@ -129,106 +131,106 @@ def StopButton():
 
 def AfterTimerOff():
     GPIO.output(GPIO26_Stop, GPIO.LOW)
-    print("Entered after timer")
-    print("STOP NO TO NC now LOW")
+    print("Stop depress\n\n")
     
+    
+
 OL_global = False
 OL_global = GPIO.input(GPIO5_OL)
+#OL_global = True
 def TurnOffOL():
     global OL_global
     OL_global=False
-    
+
 def OverLoadCheck():
-    global OL_global, OLbutton
+    global OL_global, OLbutton, stopOnceOL
     OL_global = GPIO.input(GPIO5_OL)
-    #print("###OVERLOAD CHECKER###")
-    #master.after(10000, TurnOffOL)
+    #master.after(10000, TurnOffOL)   ###### OVERLOAD TEST AUTOMATIC TURN OFF
     if OL_global == True:
         OLbutton['activebackground']="orange"
         OLbutton['bg']="orange"
-        OLbutton['text']="OVERLOAD\nDETECTED"
+        OLbutton['text']="OL\nDETECTED"
         INFOlabel['text'] = "CRITICAL: OVERLOAD DETECTED"
         Fwd_state = False
         Rev_state = False
         GPIO.output(GPIO20_Rev, GPIO.LOW)
         GPIO.output(GPIO21_Fwd, GPIO.LOW)
         GPIO.output(GPIO26_Stop, GPIO.HIGH)
-        master.after(2000, AfterTimerOff)
         FWDlabel['text'] = "OVERLOAD STOP"
         REVlabel['text'] = "OVERLOAD STOP"
         STOPlabel['text'] = "MOTOR STOP"        
     elif OL_global == False:
         OLbutton['bg']="gray"
         OLbutton['activebackground']="gray"
-        OLbutton['text']="OVERLOAD\nINDICATOR" 
+        OLbutton['text']="OVER\nLOAD" 
+        GPIO.output(GPIO26_Stop, GPIO.LOW) #turn off stop button after OL
     master.after(2000, OverLoadCheck)
 
 
 ###OVERLOAD CHECK
 master.after(2000, OverLoadCheck)
 ###OVERLOAD CHECK
-FWDbutton = tk.Button(master, text="Forward",activebackground="#4dff4d", bg="#00b300", command=FwdButton, height="3", width="6",bd=6, padx=30, pady=30, )
-FWDbutton.grid(row=0, column=0)
+FWDbutton = tk.Button(master, text="Forward",activebackground="#4dff4d", bg="#00b300", command=FwdButton, height="3", width="6",bd=6,  )
+FWDbutton.place(x=20, y=20)
 
-REVbutton = tk.Button(master, text="Reverse",activebackground="#4dff4d",bg="#00b300" , command=RevButton,height="3", width="6",bd =6, padx=30, pady=30, )
-REVbutton.grid(row=1, column=0)
+REVbutton = tk.Button(master, text="Reverse",activebackground="#4dff4d",bg="#00b300" , command=RevButton,height="3", width="6",bd =6,  )
+REVbutton.place(x=20, y=140)
 
-STOPbutton = tk.Button(master, text="STOP",activebackground="#ff1a1a", bg="#cc0000" , command=StopButton,height="3", width="6",bd =6, padx=30, pady=30, )
-STOPbutton.grid(row=2, column=0)
+STOPbutton = tk.Button(master, text="STOP",activebackground="#ff1a1a", bg="#cc0000" , command=StopButton,height="3", width="6",bd =6,  )
+STOPbutton.place(x=20, y=260)
 
-OLbutton = tk.Button(master, text="OVERLOAD\nINDICATOR",activebackground="gray", bg="gray", fg="black" ,height="3", width="6",bd =6, padx=30, pady=30, )
+OLbutton = tk.Button(master, text="OVER\nLOAD",activebackground="gray", bg="gray", fg="black" ,height="3", width="8",bd =6,  )
 #OLbutton.grid(row=2, column=7, sticky='e')
-OLbutton.place(x =500, y=200)
+OLbutton.place(x =20, y=390)
+
 FWDlabel = tk.Label(master, text="Fwd Idle", bg="black", fg="white" , justify='left')
-FWDlabel.grid(row=0, column=2, padx=20, sticky='w', )
-FWDlabel['font'] = myFont
+FWDlabel.place(x=160, y=65)
+FWDlabel['font'] = smallFont
 REVlabel = tk.Label(master, text="Rev Idle", bg="black", fg="white", justify='left')
-REVlabel.grid(row=1, column=2,padx=20, sticky='w',)
-REVlabel['font'] = myFont
+REVlabel.place(x=160, y=180)
+REVlabel['font'] = smallFont
 STOPlabel = tk.Label(master, text="Stop motor first\nbefore switching", bg="black", fg="white", justify='left' )
-STOPlabel.grid(row=2, column=2,padx=20, sticky='w',)
-STOPlabel['font'] = myFont
+STOPlabel.place(x=160, y=295)
+STOPlabel['font'] = smallFont
 
 INFOlabel = tk.Label(master, bg="black", fg="white", anchor="sw", justify='left')
-INFOlabel.grid(row=3, column=0, padx=30, pady=30,sticky='w', columnspan=10, rowspan=3)
+INFOlabel.place(x=15, y=510)
 
-NElabel = tk.Label(master, bg="black", fg="#00a3cc", anchor="ne", justify='center', text="Forward/Reverse\nMotor Controller",)
-NElabel.place(x =700, y=70)
-NElabel1 = tk.Label(master, bg="black", fg="#00a3cc", anchor="ne", justify='center', text="by Gearbulk ETO Cadets\nBATCH 3",)
-NElabel1.place(x =700, y=150)
+titleLabel = tk.Label(master, bg="black", fg="#00a3cc", anchor="ne", justify='center', text="FORWARD/REVERSE\nMOTOR CONTROLLER",)
+titleLabel.place(x =350, y=490)
+
+subTitle = tk.Label(master, bg="black", fg="#00a3cc", anchor="ne", justify='center', text="by Gearbulk ETO Cadets\nBATCH 3",)
+subTitle.place(x =400, y=550)
+
 INFOlabel['font'] = smallFont
 REVbutton['font'] = myFont
 FWDbutton['font'] = myFont
 STOPbutton['font'] = myFont
 OLbutton['font']=smallFont
-NElabel['font']=myFont
-NElabel1['font']=smallFont
+titleLabel['font']=titleFont
+subTitle['font']=smallFont
 
 gb = Image.open(logo+'gearbulk.png')
 ph = ImageTk.PhotoImage(gb)
 gearbulk = tk.Button(master, image=ph,highlightthickness=0 )
-gearbulk.place(x =700, y=250)
+gearbulk.place(x =1000, y=490, anchor='e')
 
 ntc = Image.open(logo+'NTC.png')
 ph2 = ImageTk.PhotoImage(ntc)
 ntcbutton = tk.Button(master, image=ph2,highlightthickness=0 )
-ntcbutton.place(x =700, y=320)
+ntcbutton.place(x =993, y=560, anchor='e')
 
-yong = Image.open(logo+'yong.png')
-ph3 = ImageTk.PhotoImage(yong)
-yongbutton = tk.Button(master, image=ph3,highlightthickness=0 )
-yongbutton.place(x =500, y=420)
+
 
 def my_time():
     time_string = strftime('%H:%M:%S %p \n %A \n %x') # time format 
     timeWin['text']=time_string
     timeWin.after(1000,my_time) # time delay of 1000 milliseconds 
-    print("time")
     
 
 
-timeWin=tk.Label(master,font=('Helvetica', 25, 'bold'), bg='black', fg='yellow', justify='right')
-timeWin.place(x=800, y=480)
+timeWin=tk.Label(master,font=('Helvetica', 15, 'bold'), bg='black', fg='yellow', justify='right')
+timeWin.place(x=1010, y=20, anchor='ne')
 
 my_time()
 
