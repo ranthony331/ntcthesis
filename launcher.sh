3408f891be83b6c2097aaf366363b7e7a76e661f
Reuben Anthony <ranthony331@gmail.com>
2021-11-21 21:06:06 +0800

    Added resources.txt for my future references and problems encountered

--- launcher.sh
+++ launcher.sh
@@ -3,6 +3,6 @@
 # navigate to home directory, then to this directory, then execute python script, then back home
 
 cd /
-cd home/pi/Desktop/thesis/tkinter
+cd home/pi/Desktop/thesis/main_git
 sudo python3 motor.py
 cd /
